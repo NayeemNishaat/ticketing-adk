4bf67a9d6849957867c61db6808003c94c1d26db
Nayeem Nishaat <rxnayeemimi@gmail.com>
2022-10-30 23:34:17 +0600

    added nats-streaming

---
+++ src/events/base-listener.ts
@@ -0,0 +1,53 @@
+import { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
+  // Remark: Generic type T. This is like agument for types
+  // Remark: Guideline to create listener instances
+  abstract subject: T["subject"]; // Note: Must be defined by the subclass/child class
+  abstract queueGroupName: string;
+  abstract onMessage(data: T["data"], msg: Message): void;
+  private client: Stan;
+  protected ackWait = 5 * 1000; // Note: Protected -> Subclass can define it if needed
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf8"));
+  }
+}
